21bc7e2dc7301444a2b757b05e374209528e3671
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-10-11 12:16:59 +0300

    Copy css, point to web

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -ex;
 
 mkdir -p deft
+cp ~/Dropbox/conf/pandoc/gothic.css .
 for f in ~/Dropbox/Deft/topology.txt
 do
     pandoc --standalone --mathjax -f markdown -t html \
            --metadata pagetitle="$f" \
-           --css ~/Dropbox/conf/pandoc/gothic.css \
+           --css https://k-bx.github.io/gothic.css \
            $f -o ./deft/$(basename ${f%.*}).html
 done
